df8834dc9b4dfdebb36a9810bc0580fbd1667952
choihs <choihs1998@naver.com>
2022-05-22 10:25:55 +0900

    add databaseName add at .env

--- backend/config/db.py
+++ backend/config/db.py
@@ -6,7 +6,8 @@ from pymongo import MongoClient
 load_dotenv()
 mongoUserName = os.environ.get("MONGOUSER")
 mongoPw = os.environ.get("MONGOPW")
+dbName = os.environ.get("DBNAME")
 
 MONGOURL = "mongodb+srv://" + mongoUserName +":"+quote(mongoPw)+"@cluster0.lcd2t.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(MONGOURL , serverSelectionTimeoutMS=5000)
-db = client["fastapi"]
+db = client[dbName]
